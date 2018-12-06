60bfcf6dea0745d23f43a757996e906e97bb16b7
danivivriti <danimvijay@gmail.com>
2018-12-06 20:40:18 +0530

    feat: add nodemon, error handling

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
 
 const projectRoutes = require('./api/routes/projects');
 const transactionsRoutes = require('./api/routes/transactions');
 
+app.use(morgan('dev'));
+
 app.use('/projects', projectRoutes);
 app.use('/transactions', transactionsRoutes);
+
+app.use((req, res, next) => {
+      const error = new Error('Not found');
+      error.status = 404;
+      next(error);
+});
+
+app.use((error, req, res, next) => {
+      res.status(error.status || 500);
+      res.json({
+            error: {
+                  message: error.message,
+            },
+      });
+});
 
 module.exports = app;
